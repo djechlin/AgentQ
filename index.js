66199ec30e4707b816a1fb25ddae349e79391f39
Dan <dan@unroll.me>
2014-02-21 17:47:48 -0500

    handle errors properly

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ var Q = require('q');
 
 module.exports.end = function(request) {
 	var deferred = Q.defer();
-	request.end(deferred.resolve.bind(deferred));
+	request.end(function(err, res) {
+		if(err) {
+			deferred.reject(err);
+		}
+		else if(res) {
+			deferred.resolve(res);
+		}
+		else {
+			deferred.reject("No err or res passed.");
+		}
+	});
 	return deferred.promise;
 };
+
